46004fa65d42ed2a05b577c832d7b2ea1ef9e29c
Danqing.liu <danqingx.liu@intel.com>
2012-09-20 23:41:52 +0800

    host and poll query APIs for MTW - add bean, dao, resource and hibernate configuration in AttestationService project Signed-off-by: Danqing.liu <danqingx.liu@intel.com>

---
+++ Source/AttestationService/src/com/intel/openAttestation/AttestationService/util/CommonUtil.java
@@ -0,0 +1,27 @@
+package com.intel.openAttestation.AttestationService.util;
+
+
+import java.util.HashMap;
+
+import gov.niarl.hisAppraiser.util.HisUtil;
+import com.intel.openAttestation.AttestationService.util.ActionDelay.Action;
+
+public class CommonUtil {
+	
+	private static HashMap<Action, Integer> actionIntegerHashMap = new HashMap<Action, Integer>();
+	
+	public static synchronized String generateRequestId(String label){
+		byte[] nonce = HisUtil.generateSecureRandom(16);
+		return label+ HisUtil.hexString(nonce);
+	}
+	
+	/**
+	 * Converts an Action enumeration into the related integer.
+	 * @param action Enumeration value.
+	 * @return Integer related to the enumeration.
+	 */
+	public static int getIntFromAction(Action action) {
+		return actionIntegerHashMap.get(action);
+	}
+
+}
