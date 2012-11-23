c2de9039ecc2ccaaddfc84ce87b362edcc4add4c
Xu Quan <quan.xu@intel.com>
2012-11-23 05:11:35 -0500

    make download jar package robust, selecting URL and rearchitecting downloading shell script
    Signed-off-by: Xu Quan <quan.xu@intel.com>

--- Installer/download_jar_packages.sh
+++ Installer/download_jar_packages.sh
@@ -2,55 +2,39 @@
 
 JAR_SOURCE_DIRCTORY=../JAR_SOURCE
 DOWNLOAD_LOG=Download.log
+[[ -d  $JAR_SOURCE_DIRCTORY ]] && rm -rf $JAR_SOURCE_DIRCTORY
+  mkdir $JAR_SOURCE_DIRCTORY
 
-if [ -d  $JAR_SOURCE_DIRCTORY ]
-then
-	rm -rf $JAR_SOURCE_DIRCTORY > /dev/null
-	mkdir $JAR_SOURCE_DIRCTORY
-else
-	mkdir $JAR_SOURCE_DIRCTORY
-fi
+[[ -f $DOWNLOAD_LOG ]] && rm -f $DOWNLOAD_LOG
+  touch $DOWNLOAD_LOG
 
-DOWNLOAD_CONTENT=`awk  '{print $1;}' download_jar_package_list.txt`
+DOWNLOAD_CONTENT=`cat download_jar_package_list.txt`
 
-if [ -f $DOWNLOAD_LOG ]
-then
-	rm -f $DOWNLOAD_LOG > /dev/null
-	touch $DOWNLOAD_LOG
-else
-	touch $DOWNLOAD_LOG
-fi
 for DOWNLOAD_FILE_NAME in $DOWNLOAD_CONTENT
 do
-	LOCAL_NAME=`echo "$DOWNLOAD_FILE_NAME" | awk -F "-----" '{print $1;}'`
-	DOWNLOAD_PATH=`echo "$DOWNLOAD_FILE_NAME" | awk -F "-----" '{print $2;}'`
-	echo "$LOCAL_NAME $DOWNLOAD_PATH"
-	wget -t 1 -O ../JAR_SOURCE/$LOCAL_NAME $DOWNLOAD_PATH
-	STAT=$?
-	if [ -f ../JAR_SOURCE/$LOCAL_NAME -a "$STAT" -eq 0 ]
-	then
-		if [ 0 -eq `ls -al $JAR_SOURCE_DIRCTORY/$LOCAL_NAME | awk -F " " '{print $5;}'` ] 
-		then
-			echo "Download file from $DOWNLOAD_PATH failed!" >> $DOWNLOAD_LOG
-			rm -f $JAR_SOURCE_DIRCTORY/$LOCAL_NAME
-		else
-			if [ "jar" = `ls $JAR_SOURCE_DIRCTORY/$LOCAL_NAME | awk -F "." '{print $NF}'` ]
-			then
-				ECH=`unzip -Z $JAR_SOURCE_DIRCTORY/$LOCAL_NAME | grep "uncompressed"`
-				if [ ! "$ECH" ]
-				then
-					echo "Download file from $DOWNLOAD_PATH failed!" >> $DOWNLOAD_LOG
-					rm -f $JAR_SOURCE_DIRCTORY/$LOCAL_NAME
-				fi
-			fi
-		fi
-	fi
+  LOCAL_NAME=`echo "$DOWNLOAD_FILE_NAME" | awk -F"-----" '{print $1}'`
+  DOWNLOAD_PATH=`echo "$DOWNLOAD_FILE_NAME" | awk -F"-----" '{print $2}'`
+  echo "$LOCAL_NAME $DOWNLOAD_PATH" 
+  #most downlaod times
+  key=5
+  while [ $key -gt 0 ]
+  do
+    key=$(expr $key - 1)
+    wget  -t 1 -O ../JAR_SOURCE/$LOCAL_NAME $DOWNLOAD_PATH
+    STAT=$?
+    [ $STAT == 0 ] && break
+    echo "Download file [ $LOCAL_NAME ] $(expr 5 - $key) th" >> $DOWNLOAD_LOG
+    rm -f $JAR_SOURCE_DIRCTORY/$LOCAL_NAME
+  done 
+
+  if [ $key -eq 0 ];then
+    echo "Download file [ $LOCAL_NAME ] from [ $DOWNLOAD_PATH ] failed!" >> $DOWNLOAD_LOG
+    rm -f $JAR_SOURCE_DIRCTORY/$LOCAL_NAME
+  fi
+
 done
 
-grep "Download file from http://" $DOWNLOAD_LOG
-
 cd $JAR_SOURCE_DIRCTORY
-
 cp commons-collections-2.1.1.jar commons-collections.jar 
 cp commons-logging-1.0.4.jar commons-logging.jar
 cp jaxb-impl-2.1.12.jar jaxb-impl.jar
