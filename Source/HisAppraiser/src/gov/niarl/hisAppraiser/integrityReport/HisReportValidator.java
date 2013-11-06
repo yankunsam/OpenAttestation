3314f48428d502919bccd089531ed38fd531fc63
Nicola Barresi <nbarresi@gmail.com>
2013-11-06 16:26:11 +0100

    HisAppraiser: Fixed bug in reports comparison
    This patch fixes a bug in function comparePreviousReport() of
    class HisReportValidator. The function is part of report validation
    process and compares PCRs in the current report with PCRs of last
    report received from the same host.
    
    The function comparePreviousReport() generates an error message
    when some PCR in the current report are not found in or differ from
    PCRs in the previous report. The function didn't treat the case
    of a PCR previously present and not found in the current report.

--- Source/HisAppraiser/src/gov/niarl/hisAppraiser/integrityReport/HisReportValidator.java
+++ Source/HisAppraiser/src/gov/niarl/hisAppraiser/integrityReport/HisReportValidator.java
@@ -43,7 +43,9 @@ import java.security.Signature;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.SortedSet;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 
@@ -321,10 +323,17 @@ public class HisReportValidator {
 		if (getPcrValueCount() != hisReportValidator.getPcrValueCount()) {
 			errors.add("There are a different number of PCR values for this report as compared to the previous report.");
 		}
-		for (Integer i : hisReportData.getPossiblePcrs()) {
+
+		SortedSet<Integer> possiblePcrs = new TreeSet<Integer>();
+		possiblePcrs.addAll(hisReportValidator.getHisReportData().getPossiblePcrs());
+		possiblePcrs.addAll(hisReportData.getPossiblePcrs());
+		for (Integer i : possiblePcrs) {
 			if (hisReportValidator.getPcrValue(i).length() == 0) {
 				previousReportDifferences = sb.append(DIFFERENCE_SEPARATOR).append(Integer.toString(i)).append(DIFFERENCE_SEPARATOR).toString();
 				errors.add("PCR value " + Integer.toString(i) + " is not in the previous report.");
+			} else if (getPcrValue(i).length() == 0) {
+				previousReportDifferences = sb.append(DIFFERENCE_SEPARATOR).append(Integer.toString(i)).append(DIFFERENCE_SEPARATOR).toString();
+				errors.add("PCR value " + Integer.toString(i) + " is not in the current report.");
 			} else if (!getPcrValue(i).equalsIgnoreCase(hisReportValidator.getPcrValue(i))) {
 				previousReportDifferences = sb.append(DIFFERENCE_SEPARATOR).append(Integer.toString(i)).append(DIFFERENCE_SEPARATOR).toString();
 				errors.add("PCR value " + Integer.toString(i) + " is different than the value in the previous report.");
