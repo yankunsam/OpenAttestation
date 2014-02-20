f77229dbdf6d436a065ac90ec464fddc0ed7cb0f
Nicola Barresi <nicola.barresi@polito.it>
2014-02-20 17:57:18 +0100

    HisAppraiser: Added to AttestDao the support for periodic requests
    This patch extends the AttestDao with two functions related to
    the periodic attestation requests mechanism.
    
    The function getPendingRequests() returns a list of attestation
    requests to be served by the given Client, including periodic
    requests. It will be used by the polling WS to know if there
    is a request to be served, and by the function submitReport()
    to know which request to serve with the received report.
    The function returns a list instead of a single request because
    in this way it will be possible to introduce a more complex logic
    in the process of choosing the request to be served.
    
    The function disableUnusedRequests() disables requests whose
    lastReadTime is older than configured maxIdleTime, or whose
    expirationTime is up, or whose result is UN_TRUSTED. Then it
    covers three out of the five causes of request deactivation,
    previously introduced. Deletion by user and host unreachable
    can't be managed by the Appraiser and will be covered by the
    AttestationService.
    
    Signed-off-by: Nicola Barresi <nicola.barresi@polito.it>
    Signed-off-by: Roberto Sassu <roberto.sassu@polito.it>

--- Source/HisAppraiser/src/gov/niarl/hisAppraiser/hibernate/dao/AttestDao.java
+++ Source/HisAppraiser/src/gov/niarl/hisAppraiser/hibernate/dao/AttestDao.java
@@ -14,6 +14,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 package gov.niarl.hisAppraiser.hibernate.dao;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Iterator;
 
@@ -21,12 +22,15 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 
 
+import gov.niarl.hisAppraiser.Constants;
 import gov.niarl.hisAppraiser.hibernate.domain.AttestRequest;
 import gov.niarl.hisAppraiser.hibernate.domain.MLE;
 //import gov.niarl.hisAppraiser.hibernate.domain.
 import gov.niarl.hisAppraiser.hibernate.domain.HOST_MLE;
 import gov.niarl.hisAppraiser.hibernate.domain.PcrWhiteList;
+import gov.niarl.hisAppraiser.hibernate.util.AttestUtil;
 import gov.niarl.hisAppraiser.hibernate.util.HibernateUtilHis;
+import gov.niarl.hisAppraiser.hibernate.util.ResultConverter;
 
 public class AttestDao {
 	
@@ -69,6 +73,109 @@ public class AttestDao {
 		} else {
 			return (AttestRequest) list.iterator().next();
 		}
+	}
+
+	/**
+	 * Disables requests where:
+	 *  - lastReadTime is older than configured maxIdleTime
+	 *  - expirationTime is up
+	 *  - result is UN_TRUSTED 
+	 * @param hostName Name of the machine of interest.
+	 */
+	public void disableUnusedRequests(String hostName) {
+		AttestUtil.loadProp();
+
+		String queryString = "from AttestRequest a where a.hostName = :hostName " + 
+		    "and a.threshold is not null and a.threshold >= 0 and a.expirationTime is not null";
+		Query query = HibernateUtilHis.getSession().createQuery(queryString);
+		query.setString("hostName", hostName.toLowerCase());
+
+		List list = query.list();
+		AttestRequest tmpRequest = null;
+		Iterator iter = list.iterator();
+		while (iter.hasNext()) {
+			tmpRequest = (AttestRequest) iter.next();
+
+			Long currentTime = new Date().getTime();
+			Long maxIdleTime = AttestUtil.getMaxIdleTime() * 1000 * 60 * 60 * 24;
+			boolean IDLE_EXPIRED = currentTime - tmpRequest.getLastReadTime().getTime() > maxIdleTime;
+			boolean TIME_EXPIRED = currentTime > tmpRequest.getExpirationTime().getTime();
+			boolean UN_TRUSTED = tmpRequest.getResult() != null &&
+			    tmpRequest.getResult() == ResultConverter.getIntFromResult(ResultConverter.AttestResult.UN_TRUSTED);
+
+			if (IDLE_EXPIRED || TIME_EXPIRED || UN_TRUSTED) {
+				queryString = "from AttestRequest a where a.requestId = :requestId";
+				query = HibernateUtilHis.getSession().createQuery(queryString);
+				query.setString("requestId", tmpRequest.getRequestId());
+
+				for (AttestRequest request : (List<AttestRequest>)query.list()) {
+					if (IDLE_EXPIRED)
+						request.setThreshold(Constants.PERIODIC_IDLE_EXPIRED);
+					else if (TIME_EXPIRED)
+						request.setThreshold(Constants.PERIODIC_TIME_EXPIRED);
+					else if (UN_TRUSTED)
+						request.setThreshold(Constants.PERIODIC_HOST_UNTRUSTED);
+					updateRequest(request);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Obtains a list of attestRequests to be served.
+	 * @param hostName Name of the machine of interest.
+	 * @param isConsumed The desired value for field isConsumedByPollingWS
+	 * @return A list of AttestRequest entries to be served
+	 */
+	public List<AttestRequest> getPendingRequests(String hostName, boolean isConsumed) {
+		List<AttestRequest> requestList = new ArrayList<AttestRequest>();
+		AttestUtil.loadProp();
+
+		String queryString = "from AttestRequest a where a.hostName = :hostName and " +
+		                     "((a.isConsumedByPollingWS = :isConsumedByPollingWS and a.auditLog is null and a.result is null) " +
+		                     "or (a.threshold is not null and a.threshold >= 0)) order by a.validateTime asc";
+		Query query = HibernateUtilHis.getSession().createQuery(queryString);
+		query.setString("hostName", hostName.toLowerCase());
+		query.setBoolean("isConsumedByPollingWS", isConsumed);
+
+		List list = query.list();
+		AttestRequest tmpRequest = null;
+		Iterator iter = list.iterator();
+		while (iter.hasNext()) {
+			tmpRequest = (AttestRequest) iter.next();
+
+			if (tmpRequest.getThreshold() == null || tmpRequest.getValidateTime() == null ||
+			    tmpRequest.getCurrentProcessingTime() == null) {
+				requestList.add(tmpRequest);
+				continue;
+			}
+
+			Long currentTime = new Date().getTime();
+			Long lastValidateTime = tmpRequest.getValidateTime().getTime();
+
+			/*
+			 * If requestTime is greater than validateTime the
+			 * request is currently being processed by the
+			 * Appraiser. Then if isConsumed is true, the request
+			 * can be directly added to the requestList; otherwise
+			 * it has to be skipped
+			 */
+			if (lastValidateTime < tmpRequest.getRequestTime().getTime()) {
+				if (isConsumed)
+					requestList.add(tmpRequest);
+				continue;
+			}
+
+			if ((currentTime - lastValidateTime) > AttestUtil.getMinAttestInterval() &&
+			    (currentTime - lastValidateTime) > tmpRequest.getThreshold() - tmpRequest.getCurrentProcessingTime() * AttestUtil.getAnticipationFactor()) {
+				requestList.add(tmpRequest);
+			}
+		}
+
+		if (requestList.size() == 0)
+			requestList.add(new AttestRequest());
+
+		return requestList;
 	}
 	
 	/**
