17ca02ebe7cf03f0c893715542691d0fcf4e18aa
Quan Xu <quan.xu@intel.com>
2014-07-06 23:41:25 -0400

    add deploy script ..
    Signed-off-by: Quan Xu <quan.xu@intel.com>

---
+++ deploy/deploy.sh
@@ -0,0 +1,224 @@
+##parase config to get param value
+
+### configuration ###
+oat_server_ip="___ IP ___"
+mysql_password="______ MySQL PASSPE ______"
+saml_password="samlpasswd2"
+portal_password="portalpasswd2"
+p12_password="p12passwd2"
+
+###########
+TOP_DIR=$(cd .. && pwd)
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+
+conf_dir=${conf_dir:-/etc/intel/cloudsecurity}
+[ -d $conf_dir ] && rm -rf $csonf_dir
+install -d  $conf_dir
+
+tomcat_dir=${tomcat_dir:-/var/lib/tomcat6}
+oat_home_dir=${oat_home_dir:-$HOME/.oat}
+[ -d $oat_home_dir ] && rm -rf $oat_home_dir
+install -d  $oat_home_dir
+
+###MySQL ### 
+mysql -uroot -p$mysql_password -e 'create database mw_as';
+mysql -uroot -p$mysql_password mw_as < \
+$TOP_DIR/database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/mtwilson.sql
+
+cd $conf_dir
+mtwilson_properties()
+{
+   cat <<'EOF' > "$conf_dir/mtwilson.properties"
+mtwilson.api.baseurl=https://OATAPPR_IP:8181
+mtwilson.api.ssl.policy=TRUST_FIRST_CERTIFICATE
+mtwilson.db.driver=com.mysql.jdbc.Driver
+mtwilson.db.url=jdbc:mysql://localhost/mw_as
+mtwilson.db.user=root
+mtwilson.db.password=MYSQL_PASSWD
+EOF
+}
+
+trust_properties()
+{
+   cat <<'EOF' > "$conf_dir/trust-dashboard.properties"
+mtwilson.tdbpkeystore.dir=/etc/intel/cloudsecurity
+mtwilson.tdbp.keystore.password=PORTAL_PASSWD
+imagesRootPath= images/
+trustUnknow = images/Unknown.png
+trustTure = images/Trusted.png
+trustFalse = images/UnTrusted.png
+ubuntu = images/ubuntu.png
+vmware = images/vmware.png
+suse = images/suse.png
+kvm = images/kvm.png
+xen = images/xen.png
+mtwilson.tdbp.sessionTimeOut = 1800
+mtwilson.tdbp.paginationRowCount = 10
+EOF
+}
+
+
+whitelist_properties()
+{
+   cat <<'EOF' > "$conf_dir/whitelist-portal.properties"
+mtwilson.wlmp.keystore.dir=/etc/intel/cloudsecurity
+mtwilson.wlmp.keystore.password=PORTAL_PASSWD
+mtwilson.wlmp.openSourceHypervisors=KVM;Xen
+mtwilson.wlmp.sessionTimeOut=1800
+mtwilson.wlmp.pagingSize=8
+EOF
+}
+
+
+attestation_properties()
+{
+   cat <<'EOF' > "$conf_dir/attestation-service.properties"
+com.intel.mountwilson.as.trustagent.timeout=3
+com.intel.mountwilson.as.attestation.hostTimeout=60
+com.intel.mountwilson.as.home=/var/opt/intel/aikverifyhome
+com.intel.mountwilson.as.aikqverify.cmd=aikqverify
+com.intel.mountwilson.as.openssl.cmd=openssl.sh
+saml.key.aslias=samlkey1
+saml.keystore.file=SAML.jks
+saml.keystore.password=SAML_KEYSTORE_PASSWD
+saml.validity.seconds=3600
+saml.issuer=https://OATAPPR_IP:8181
+saml.key.password=SAML_KEY_PASSWD
+privacyca.server=OATAPPR_IP
+com.intel.mtwilson.as.buisiness.trust.sleepTime=1
+EOF
+}
+
+privacyca_client_properties()
+{
+   cat <<'EOF' > "$conf_dir/privacyca-client.properties"
+PrivacyCaUrl=https://OATAPPR_IP:8181/HisPrivacyCAWebServices2
+PrivacyCaSubjectName=HIS_PRIVACY_CA
+PrivacyCaPassword=***replace***
+EndorsementCaSubjectName=Endorsement_CA_Rev_1
+EndorsementCaPassword=***replace***
+CertValidityDays=3652
+AikAuth=1111111111111111111111111111111111111111
+ecStorage=NVRAM
+ecSigningKeySize=2048
+ecLocation=/opt/intel/cloudsecurity/trustagent
+TpmOwnerAuth=1111111111111111111111111111111111111111
+EOF
+}
+
+
+privacyca_properties()
+{
+   cat <<'EOF' > "$conf_dir/PrivacyCA.properties"
+ClientFilesDownloadUsername=admin
+ClientFilesDownloadPassword=PRIVACY_CA_PASSWD
+EOF
+}
+
+mtwilson_properties
+trust_properties
+whitelist_properties
+attestation_properties
+privacyca_properties
+privacyca_client_properties
+
+sed -i "s/OATAPPR_IP/$oat_server_ip/g" $conf_dir/mtwilson.properties
+sed -i "s/OATAPPR_IP/$oat_server_ip/g" $conf_dir/attestation-service.properties
+sed -i "s/MYSQL_PASSWD/$mysql_password/g" $conf_dir/mtwilson.properties
+sed -i "s/PORTAL_PASSWD/$portal_password/g" $conf_dir/trust-dashboard.properties
+sed -i "s/PORTAL_PASSWD/$portal_password/g" $conf_dir/whitelist-portal.properties
+sed -i "s/SAML_KEYSTORE_PASSWD/$saml_password/g" $conf_dir/attestation-service.properties
+sed -i "s/SAML_KEY_PASSWD/$saml_password/g" $conf_dir/attestation-service.properties
+sed -i "s/PRIVACY_CA_PASSWD/$p12_password/g" $conf_dir/PrivacyCA.properties
+
+att_parse()
+{
+  cat $conf_dir/attestation-service.properties | \
+      grep "$1" | awk -F= '{print $2}'
+}
+
+trust_tdb_parse()
+{
+  cat $conf_dir/trust-dashboard.properties | \
+      grep "$1" | awk -F= '{print $2}'
+}
+
+saml_key_aslias="`att_parse "saml.key.aslias"`"
+saml_keystore_file="`att_parse "saml.keystore.file"`"
+saml_keystore_password="`att_parse "saml.keystore.password"`"
+saml_key_password="`att_parse "saml.key.password"`"
+server_keystore_password="`date  +%s%N`"
+server_key_password=$server_keystore_password
+
+echo ${saml_key_aslias}
+echo ${saml_keystore_file}
+echo ${saml_keystore_password}
+echo ${saml_key_password}
+
+### Create SAML Signing Key ###
+keytool -genkey -alias $saml_key_aslias -keyalg RSA -keysize 2048 \
+        -keystore $saml_keystore_file -storepass $saml_keystore_password \
+        -dname "CN=OpenAttestation, O=My Org, OU=Mt Wilson, C=US" \
+        -validity 3650 -keypass $saml_key_password
+
+keytool -export -alias $saml_key_aslias -keystore $saml_keystore_file \
+        -storepass $saml_keystore_password -file $oat_home_dir/saml.crt
+
+### Create EK Signing Certificate ###
+find $TOP_DIR -name "HisPrivacyCAWebServices2*-setup.jar" | \
+      xargs -i java -jar {} 
+cp $conf_dir/clientfiles/PrivacyCA.cer $conf_dir
+
+### Create Attestation Server Certificate ###
+privacyca_server="`att_parse "privacyca.server"`"
+install -d $tomcat_dir/Certificate
+[ -e $tomcat_dir/Certificate/keystore.jks ] && \
+     rm -f $tomcat_dir/Certificate/keystore.jks
+
+[ -e $tomcat_dir/Certificate/ssl.${privacyca_server}.crt ] && \
+     rm -f $tomcat_dir/Certificate/ssl.${privacyca_server}.crt
+
+keytool -genkey -alias s1as -keyalg RSA -keysize 2048 \
+        -keystore $oat_home_dir/keystore.jks -storepass $server_keystore_password \
+        -dname "CN=${privacyca_server}, OU=Mt Wilson, O=My Org, C=US" \
+        -validity 3650 -ext san=IP:${privacyca_server} \
+        -keypass $server_key_password
+
+keytool -exportcert -alias s1as -keystore $oat_home_dir/keystore.jks \
+        -storepass $server_keystore_password \
+        -file $oat_home_dir/ssl.${privacyca_server}.crt
+
+cp -f $oat_home_dir/keystore.jks /var/lib/tomcat6/Certificate/
+cp -f $oat_home_dir/ssl.${privacyca_server}.crt /var/lib/tomcat6/Certificate/
+
+### Create Portal Signing Key ###
+tdbp_password="`trust_tdb_parse "mtwilson.tdbp.keystore.password"`"
+
+keytool -genkey -alias admin -keyalg RSA -keysize 2048 -keystore $oat_home_dir/portal.jks \
+        -storepass $tdbp_password "CN=Portal User, OU=Mt Wilson, O=My Org, C=US" \
+        -validity 3650 -keypass $tdbp_password
+
+keytool -importcert -file $oat_home_dir/saml.crt -keystore $oat_home_dir/portal.jks \
+        -storepass $tdbp_password -alias "mtwilson (saml)"
+
+keytool -importcert -file $oat_home_dir/ssl.${privacyca_server}.crt  \
+        -keystore $oat_home_dir/portal.jks \
+        -storepass $tdbp_password -alias "mtwilson (ssl)"
+
+sed -i "s/ <\/Service>/<Connector port=\"8181\" minSpareThreads=\"5\" maxSpareThreads=\"75\" enableLookups=\"false\" disableUploadTimeout=\"true\" acceptCount=\"100\" maxThreads=\"200\" scheme=\"https\" secure=\"true\" SSLEnabled=\"true\" clientAuth=\"want\" sslProtocol=\"TLS\" ciphers=\"TLS_ECDH_anon_WITH_AES_256_CBC_SHA, TLS_ECDH_anon_WITH_AES_128_CBC_SHA, TLS_ECDH_anon_WITH_3DES_EDE_CBC_SHA, TLS_ECDH_RSA_WITH_AES_256_CBC_SHA, TLS_ECDH_RSA_WITH_AES_128_CBC_SHA, TLS_ECDH_RSA_WITH_3DES_EDE_CBC_SHA, TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA, TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA, TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA, TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA, TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA, TLS_ECDH_ECDSA_WITH_3DES_EDE_CBC_SHA, TLS_DHE_RSA_WITH_AES_256_CBC_SHA, TLS_DHE_DSS_WITH_AES_256_CBC_SHA, TLS_RSA_WITH_AES_256_CBC_SHA, TLS_DHE_RSA_WITH_AES_128_CBC_SHA, TLS_DHE_DSS_WITH_AES_128_CBC_SHA, TLS_RSA_WITH_AES_128_CBC_SHA\" keystoreFile=\"Certificate\/keystore.jks\" keystorePass=\"$server_keystore_password\" truststoreFile=\"Certificate\/keystore.jks\" truststorePass=\"$server_key_password\" \/><\/Service>/g" /var/lib/tomcat6/conf/server.xml
+### copy oat war packages ###
+copy_war_package()
+{
+    find $TOP_DIR -name "$1*.war" | \
+        xargs -i cp {} $tomcat_dir/webapps/$1.war
+}
+
+copy_war_package "HisPrivacyCAWebServices2"
+copy_war_package "WLMService"
+copy_war_package "WhiteListPortal"
+copy_war_package "AttestationService"
+copy_war_package "TrustDashBoard"
+
+### tomcat6 restart ###
+service tomcat6 restart
+
