cf60c43ea2c373523848b4e00e4bde17e216c8d2
Quan Xu <quan.xu@intel.com>
2014-11-21 01:07:14 -0500

    add deploy_sles.sh and undeploy_sles.sh for SLES ..
    Signed-off-by: Quan Xu <quan.xu@intel.com>

---
+++ deploy/undeploy_sles.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#undeploy OpenAttestation 2.x
+
+mysql_password="______ MySQL PASSWD ______"
+
+conf_dir=${conf_dir:-/etc/intel/cloudsecurity}
+tomcat_dir=${tomcat_dir:-/usr/share/tomcat}
+oat_home_dir=${oat_home_dir:-$HOME/.oat}
+
+delete()
+{
+  rm -rf $1
+}
+
+delete "$conf_dir/*"
+delete "$oat_home_dir/*"
+
+mysql -uroot -p$mysql_password -e 'drop database mw_as';
+line="`grep -n "<\/Service>" $tomcat_dir/conf/server.xml | \
+       awk -F: '{print $1}'`"
+line=$[$line - 1]
+str="`sed -n "$line p" $tomcat_dir/conf/server.xml | grep "Connector"`"
+[[ ! -z $str ]] && \
+    sed -i "$line d" $tomcat_dir/conf/server.xml
+
+delete "$tomcat_dir/webapps/HisPrivacyCAWebServices2*"
+delete "$tomcat_dir/webapps/WLMService*"
+delete "$tomcat_dir/webapps/WhiteListPortal*"
+delete "$tomcat_dir/webapps/AttestationService*"
+delete "$tomcat_dir/webapps/TrustDashBoard*"
