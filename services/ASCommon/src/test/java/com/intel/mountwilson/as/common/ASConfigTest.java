16f390d23bd57a9a8010d429e40904e1b159711d
Emily <lijuan.zhang@intel.com>
2014-11-30 18:20:35 +0800

    bugfix testcase in ASCommon
    Signed-off-by: Emily <lijuan.zhang@intel.com>

--- services/ASCommon/src/test/java/com/intel/mountwilson/as/common/ASConfigTest.java
+++ services/ASCommon/src/test/java/com/intel/mountwilson/as/common/ASConfigTest.java
@@ -24,43 +24,6 @@ public class ASConfigTest {
     }
     
     @Test
-    public void testGetJpaProperties(){
-        Properties prop = ASConfig.getJpaProperties();
-        
-        //TODO: Not care about the value defined in current ENV, will cover the relevant logic in test suites for ConfigBase.java
-        /*
-        InputStream inputStream = this.getClass().getResourceAsStream("/" + "attestation-service.properties");
-        Properties expectedProp = new Properties();
-        try {
-            if (inputStream != null) {
-                expectedProp.load(inputStream);
-                
-            } else {
-                expectedProp.load(new FileInputStream("/etc/intel/cloudsecurity/" + "attestation-service.properties"));
-                expectedProp.load(new FileInputStream("/etc/intel/cloudsecurity/mtwilson.properties"));
-            }
-        } catch (IOException ex) {
-            System.out.println("Encounter an exception, detail message: " + ex.toString());
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException ex) {
-                    System.out.println("Encounter an exception, detail message: " + ex.toString());
-                }
-            }
-        }
-        */
-        
-        //Just compare with default value.
-        //It's not surprising to know some test cases listed below will unable to pass verification in the env has been deployed with OAT previously 
-        Assert.assertEquals(prop.getProperty("javax.persistence.jdbc.driver"), "com.mysql.jdbc.Driver");
-        Assert.assertEquals(prop.getProperty("javax.persistence.jdbc.url"), "jdbc:mysql://127.0.0.1:3306/mw_as?autoReconnect=true");
-        Assert.assertEquals(prop.getProperty("javax.persistence.jdbc.user"), "root");
-        Assert.assertEquals(prop.getProperty("javax.persistence.jdbc.password"), "password");
-    }
-    
-    @Test
     public void testGetTrustAgentTimeOutinMilliSecs(){
         Assert.assertEquals(ASConfig.getTrustAgentTimeOutinMilliSecs(), 3000);
     }
