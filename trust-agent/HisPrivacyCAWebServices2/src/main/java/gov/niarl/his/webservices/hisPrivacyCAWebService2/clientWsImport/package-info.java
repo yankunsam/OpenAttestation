6a88b1e1a501237c9a0f7b1c48de56feb1a3ace5
zaaquino <zahedi.aquino@intel.com>
2015-12-17 17:23:53 -0600

    Pushing just tboot code

--- trust-agent/HisPrivacyCAWebServices2/src/main/java/gov/niarl/his/webservices/hisPrivacyCAWebService2/clientWsImport/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://server.hisPrivacyCAWebService2.webservices.his.niarl.gov/")
-package gov.niarl.his.webservices.hisPrivacyCAWebServices2.clientWsImport;
